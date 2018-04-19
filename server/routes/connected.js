8bcac1b8036227750890c7410f5e96a5785a5fd0
annie <atruong32@gatech.edu>
2018-04-19 15:57:51 -0400

    untracking app.js and localhost database configuration

--- server/routes/connected.js
+++
@@ -1,9 +0,0 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-   host     : 'localhost',
-   user     : 'root',
-   password : '0123',
-   database : 'cs4400'
- });
-
-module.exports =connection;
