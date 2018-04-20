8e3b649a5b47d1a7d1fc89a101dad5d0e962cf9d
annie <atruong32@gatech.edu>
2018-04-20 17:16:08 -0400

    removed routes.js

--- server/routes/connected.js
+++ server/routes/connected.js
@@ -2,8 +2,8 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
-   database : 'cs4400',
-   password : '0123'
+   database : 'temp1',
+   password : ''
  });
 
 module.exports =connection;
