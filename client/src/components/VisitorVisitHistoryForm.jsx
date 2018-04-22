c3ea13f13896cd2db33406671948991fd8bd1497
K <kiettran8795@gmail.com>
2018-04-22 05:24:57 -0400

    Update visitor view of visit history

--- client/src/components/VisitorVisitHistoryForm.jsx
+++ client/src/components/VisitorVisitHistoryForm.jsx
@@ -2,39 +2,110 @@ import React, { PropTypes } from 'react'; import { Link } from 'react-router';
 import { Card, CardText } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import ViewVisitHistoryTableForm from '../AdminTableManagement/ViewVisitHistoryTableForm';
+
+
+import { BootstrapTable, TableHeaderColumn, InsertButton, DeleteButton  } from 'react-bootstrap-table';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+
+import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import 'react-notifications/lib/notifications.css';
 
 
 const VisitorVisitHistoryForm = ({
-  onSubmit,
-  onChange,
-  errors,
-  user
-}) => (
-  <Card className="container">
-    <form action="/" onSubmit={onSubmit}>
-      <h2 className="card-heading">Welcome Admin1</h2>
+  onViewProperty,
+  onAddSelectedRow,
 
+  // onSubmit,
+  // onChange,
+  errors,
+  data
+}) => (
+  
+  <Card className="container" expandable = {true} >
+    {/* <form action="/" onSubmit = {onSubmit}> */}
+      <h2 className="card-heading">Your Visit History</h2>
       {errors.summary && <p className="error-message">{errors.summary}</p>}
 
+      <BootstrapTable data={ data }
+                        selectRow={ {
+                          mode: 'radio',
+                          bgColor: '#4285F4',
+                          hideSelectColumn: true,
+                          clickToSelect: true,
+                          onSelect: onAddSelectedRow
+                    
+                        }}
+                        search pagination
+                        options={ {
+                          expandBy: 'column',
+                          clearSearch:true
+                        } } 
+                        hover height='100%'
+                        printable
+                        version='4'
+        >
+
+          <TableHeaderColumn 
+            isKey={ true }
+            dataField='Name' 
+            dataSort={ true } 
+            dataAlign='center'
+            width='15%'
+            tdStyle={ { whiteSpace: 'normal' } } 
+            thStyle={ { whiteSpace: 'normal' } }
+          >
+          Name
+          </TableHeaderColumn>
+          
+          <TableHeaderColumn 
+            dataField='Date' 
+            dataSort={ true } 
+            dataAlign='center'
+            width='10%'
+            tdStyle={ { whiteSpace: 'normal' } } 
+            thStyle={ { whiteSpace: 'normal' } }
+          >
+          Date Logged
+          </TableHeaderColumn>
+        
+          <TableHeaderColumn 
+            dataField='rating' 
+            dataSort={ true } 
+            dataAlign='center'
+            width='8%'
+            tdStyle={ { whiteSpace: 'normal' } } 
+            thStyle={ { whiteSpace: 'normal' } }
+
+          >
+          Rating
+          </TableHeaderColumn>
+        </BootstrapTable>
+
       <div className="button-line">
-        <RaisedButton type="view_property_details" label="View Property Details" primary 
-                      href="./PropertyDetails"/>
+        <RaisedButton 
+          type="view_property_details" 
+          label="View Property Details" primary 
+          onClick={onViewProperty}
+        />
       </div>
 
       <div className="button-line">
         <RaisedButton type="back" label="Back" primary         
                       href="./DashBoard"/>
       </div>
-
-    </form>
+    {/* </form> */}
   </Card>
 );
 
 VisitorVisitHistoryForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
+  onViewProperty: PropTypes.func.isRequired,
+  onAddSelectedRow: PropTypes.func,
+
+  // onSubmit: PropTypes.func.isRequired,
+  // onChange: PropTypes.func.isRequired,
   errors: PropTypes.object.isRequired,
-  user: PropTypes.object.isRequired
+  data: PropTypes.array.isRequired
 };
 
 export default VisitorVisitHistoryForm;
--- client/src/helpers/DataPopulation.js
+++ client/src/helpers/DataPopulation.js
@@ -42,3 +42,46 @@ export function getFarmItems() {
     })
   })
 }
+
+export function getVisitHistory(Username) {
+  console.log('function getVisitHistory is running => ');
+  console.log('Visitor is ', Username);
+  return new Promise(function(resolve, reject) {
+    axios.get(`/populate/getVisitHistory/${Username}`)
+    .then(function (res) {
+      console.log("Ahihihihihihiiiiiii");
+
+      if (res.data.Error) {
+        console.log('res.data.errors => ',res.data.errors);
+        reject(res.data.Error)
+      } else {
+        // console.log(res);
+        // var data = JSON.parse(res.data.properties);
+        console.log(res.data.properties);
+        resolve(res.data.properties);
+      }
+    })
+  })
+}
+
+
+export function getDetailProperty(name) {
+  console.log('function getDetailProperty is running => ');
+  console.log('Property is ', Username);
+  return new Promise(function(resolve, reject) {
+    axios.get(`/populate/getDetailProperty/${name}`)
+    .then(function (res) {
+      console.log("Ahihihihihihiiiiiii");
+
+      if (res.data.Error) {
+        console.log('res.data.errors => ',res.data.errors);
+        reject(res.data.Error)
+      } else {
+        // console.log(res);
+        // var data = JSON.parse(res.data.properties);
+        console.log(res.data.properties);
+        resolve(res.data.properties);
+      }
+    })
+  })
+}
