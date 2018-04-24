7b8bd92cc8281b3fd5ea590885884c02b76d66f2
Chau Phan <chauphan0801@gmail.com>
2018-04-24 02:25:57 -0400

    Add data to visit table

--- SQL/visit.sql
+++ SQL/visit.sql
@@ -18,4 +18,28 @@ INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("kellis", "0
 
 INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("ashton.woods", "00002", "2017-10-27", "5");
 
-INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("adinozzo", "00003", "2017-10-10", "1");
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("cecephan", "00003", "2017-08-09", "3");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("cecephan", "00001", "2017-10-10", "1");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("cecephan", "00005", "2017-11-01", "5");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("roseymisey", "00002", "2017-09-25", "5");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("roseymisey", "00001", "2017-01-08", "1");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("floridaorange", "00003", "2017-10-10", "4");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("floridaorange", "00001", "2017-07-07", "1");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("floridaorange", "00005", "2017-05-20", "2");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("thesenuts", "00001", "2018-03-14", "1");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("thesenuts", "00002", "2018-09-08", "5");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("anqtran", "00005", "2017-06-16", "2");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("anqtran", "00001", "2017-04-08", "3");
+
+INSERT INTO Visit (Username, PropertyID, VisitDate, Rating) VALUES ("anqtran", "00003", "2017-10-10", "5");
