91cb8acaa4035c14c4c147f7ffbd9b3a24e17449
K <kiettran8795@gmail.com>
2018-04-24 03:18:01 -0400

    Update Visitor Dashboard

--- client/src/AdminTableManagement/ViewDetailPropertyForm.jsx
+++ client/src/AdminTableManagement/ViewDetailPropertyForm.jsx
@@ -11,26 +11,14 @@ const ViewDetailPropertyForm = ({
 }) => (
   <Card className="container">
     <form>
-    {data[1]}
-      <h3> Name: {data[0].Name} </h3> 
-      <h3> Owner: {data[0].Name}</h3>
-      <h3> Owner Email: {data[0].Name}</h3>
-      <h3> Visits: {data[0].numberofVisit}</h3>
-      <h3> Address: {data[0].Street}</h3>
-      <h3> City: {data[0].City}</h3>
-      <h3> Zip: {data[0].Zip}</h3>
-      <h3> Size (acres): {data[0].Size}</h3>
-      <h3> Avg. Rating: {data[0].avgRating}</h3>
-      <h3> Type: {data[0].PropertyType}</h3>
-      <h3> Public: {data[0].IsPublic}</h3>
-      <h3> Commercial: {data[0].IsCommercial}</h3>
-      <h3> ID: {data[0].ID}</h3>
-      <form oninput="cropList.value=data[2] !== '' ? '':'Crops: ' + data[2]  ">
-        <output name="cropList"></output>
-      </form>
-      <form oninput="animalList.value=data[1] !== '' ? '':'Animals: ' + data[1]  ">
-        <output name="animalList"></output>
-      </form>
+    <table>
+      <tr>
+        <td>Name :  </td>
+
+        <td>data[0].Name  </td>
+      </tr>
+    </table>
+
 
     </form>
   </Card>
